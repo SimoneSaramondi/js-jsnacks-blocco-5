9664a4481a5ecec19c9b918f7a95f8a965c0d0d3
SimoneSaramondi <simo98sara@gmail.com>
2021-04-23 17:47:52 +0200

    Start

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+[Snack 1]
+Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà,
+peso e lunghezza. Calcola quanto pesano tutte le zucchine.
+
+[Snack 2]
+Crea 10 oggetti che rappresentano una zucchina.
+Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+*/
